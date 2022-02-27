d6d5a98ecb454250f0e6325ddadf3d2b624e87bd
Serg Buglakov <sbuglakov@xiag.ch>
2022-02-27 22:41:54 +0700

    convert to swift package manager

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "network-tester",
+    platforms: [
+        .macOS(.v12),
+    ],
+    products: [
+        .executable(name: "network-tester", targets: ["network-tester"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/samiyr/SwiftyPing.git", branch: "master")
+    ],
+    targets: [
+        .executableTarget(
+            name: "network-tester",
+            dependencies: ["SwiftyPing"]),
+    ]
+)
