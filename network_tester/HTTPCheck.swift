bee3ff421bb8239371c00a1dcd9faeb86755be51
Alexandra <marajade@ngs.ru>
2023-01-04 22:35:00 +0200

    simple http checks

---
+++ network_tester/HTTPCheck.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+class HTTPCheck: CheckProtocol {
+    var status: CheckStatus
+    var request: URLRequest
+
+    init(url: URL) {
+        request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        status = CheckStatus.notLaunchedYet
+    }
+
+    func performCheck() {
+        status = .inProgress
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                self.status = .failed
+                return
+            }
+            self.status = .success
+        }
+        task.resume()
+    }
+
+}
