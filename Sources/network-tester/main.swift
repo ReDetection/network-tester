4da3af34845b1160eec4180fd6824b550e76e39d
Alexandra Mirzuitova <marajade@ngs.ru>
2023-01-16 22:15:48 +0200

    Transformed a single check into an array of checks and moved callback adding to a function

--- Sources/network-tester/main.swift
+++ Sources/network-tester/main.swift
@@ -1,14 +1,31 @@
 import Foundation
 
 var finished = false
-let check = HTTPCheck(url: URL(string: "https://google.com")!)
-check.callback = {
-    print(check.status)
-    finished = true
-}
-check.performCheck()
+var checks: [CheckProtocol] = []
 
-while !finished{
+let googleCheck = HTTPCheck(url: URL(string: "https://google.com")!)
+let checkHomeAssistant = HTTPCheck(url: URL(string: "http://192.168.21.217/")!)
+checks.append(googleCheck)
+checks.append(checkHomeAssistant)
+
+for check in checks {
+    addCallback(check: check)
+    finished = false
+    check.performCheck()
+
+    while !finished{
     _ = RunLoop.main.run(mode: .default, before: Date(timeIntervalSinceNow: 0.5))
+    }
 }
 
+print("checks all done")
+
+func addCallback(check: CheckProtocol){
+    check.callback = {
+        if let httpCheck = check as? HTTPCheck{
+            print("checking URL " + httpCheck.request.url!.absoluteString)
+        }
+        print(check.status)
+        finished = true
+    }
+}
