ad5332f381b54955d389bfbd1b417603d2655bb4
Serg <redetection@gmail.com>
2025-02-05 00:40:29 +0200

    Useful set of checks

--- side-apps/NetTesterApp/NetTesterApp/ViewController.swift
+++ side-apps/NetTesterApp/NetTesterApp/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
         checks = [
             HotspotCheck().named("Internet/hotspot detection"),
             HTTPCheck(url: URL(string: "https://ya.ru")!).named("Yandex availability"),
+            HTTPCheck(url: URL(string: "http://192.168.21.149/")!, expectedStatusCode: 404).named("Docker server by IP"),
+            HTTPCheck(url: URL(string: "http://pihole.local/admin/")!).named("DNS pihole.local"),
+            HTTPCheck(url: URL(string: "http://homeassistant.local:8123")!).named("homeassistant.local"),
         ]
 
         for check in checks {
